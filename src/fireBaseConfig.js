95297c121837975bb5b0bfec62a94f14c6c5ef13
Andrei Tosa <deiu.tosa@gmail.com>
2019-07-15 20:01:17 +0300

    Delete fireBaseConfig.js

--- src/fireBaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyDkbzm_nLzJ50gyLU0q-GMpnx8cAhm1bk8",
-  authDomain: "andrei-resume.firebaseapp.com",
-  databaseURL: "https://andrei-resume.firebaseio.com",
-  projectId: "andrei-resume",
-  storageBucket: "",
-  messagingSenderId: "57727479863",
-  appId: "1:57727479863:web:a052e046b555397e"
-};
